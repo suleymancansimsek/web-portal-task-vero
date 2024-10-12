3e7799c9938970796117ff4d92e690c2d13c2179
suleymancansimsek <suleymancansimsek@gmail.com>
2024-10-12 18:09:10 +0300

    using cdn for jquery

--- pages/table.php
+++ pages/table.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     
-    <script src="../vendor/components/jquery/jquery.min.js" type="text/javascript"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
     <title>Task List</title>
 </head>
